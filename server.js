eac3fdfab9d6a046d04dca1616df6833278c802a
Andrew Lee <andyrewlee@gmail.com>
2015-08-20 14:46:44 -0700

    Final adjustments

--- server.js
+++ server.js
@@ -12,4 +12,20 @@ var io = require('socket.io').listen(server);
 io.sockets.on('connection', function(socket) {
   console.log("Connection from...");
   console.log(socket.id); 
+
+  socket.on("A", function() {
+    io.sockets.emit("A");
+  });
+
+  socket.on("B", function() {
+    io.sockets.emit("B");
+  });
+
+  socket.on("C", function() {
+    io.sockets.emit("C");
+  });
+
+  socket.on("D", function() {
+    io.sockets.emit("D");
+  });
 });
